2ee2b08d3dab97cd099d28a24e88766a6084489e
defnngj <defnngj@gmail.com>
2022-05-04 21:31:52 +0800

    fix: 用例断言错误

--- backend/app_api/running_tests.py
+++ backend/app_api/running_tests.py
@@ -64,6 +64,7 @@ class RunTest(unittest.TestCase):
     """
     @file_data(DATA_FILE_PATH)
     def test_case(self, url, method, header, params_type, params_body, assert_type, assert_text):
+        print("\n======interface info==========")
         print("url", url)
         print("method", method)
         print("header", header)
@@ -71,6 +72,7 @@ class RunTest(unittest.TestCase):
         print("params_body", params_body)
         print("assert_type", assert_type)
         print("assert_text", assert_text)
+        print("======interface info==========")
 
         header = json_to_dict(header)
         if header is None:
@@ -81,7 +83,7 @@ class RunTest(unittest.TestCase):
             params_body = {}
 
         if method == MethodType.get:
-            ret_text = requests.get(url, headers=header, params=params_body)
+            ret_text = requests.get(url, headers=header, params=params_body).text
             if assert_type == AssertType.include:
                 self.assertIn(assert_text, ret_text)
             elif assert_type == AssertType.equal:
@@ -91,16 +93,15 @@ class RunTest(unittest.TestCase):
 
         if method == MethodType.post:
             if params_type == ParamsType.data:
-                ret_text = requests.post(url, headers=header, data=params_body)
+                ret_text = requests.post(url, headers=header, data=params_body).text
                 if assert_type == AssertType.include:
-                    print("ret_text--->", ret_text)
                     self.assertIn(assert_text, ret_text)
                 elif assert_type == AssertType.equal:
                     self.assertEqual(assert_text, ret_text)
                 else:
                     pass
             if params_type == ParamsType.json:
-                ret_text = requests.post(url, headers=header, json=params_body)
+                ret_text = requests.post(url, headers=header, json=params_body).text
                 if assert_type == AssertType.include:
                     self.assertIn(assert_text, ret_text)
                 elif assert_type == AssertType.equal:
@@ -110,7 +111,7 @@ class RunTest(unittest.TestCase):
 
         if method == MethodType.put:
             if params_type == ParamsType.data:
-                ret_text = requests.put(url, headers=header, data=params_body)
+                ret_text = requests.put(url, headers=header, data=params_body).text
                 if assert_type == AssertType.include:
                     self.assertIn(assert_text, ret_text)
                 elif assert_type == AssertType.equal:
@@ -119,7 +120,7 @@ class RunTest(unittest.TestCase):
                     pass
 
             if params_type == ParamsType.json:
-                ret_text = requests.put(url, headers=header, json=params_body)
+                ret_text = requests.put(url, headers=header, json=params_body).text
                 if assert_type == AssertType.include:
                     self.assertIn(assert_text, ret_text)
                 elif assert_type == AssertType.equal:
@@ -129,7 +130,7 @@ class RunTest(unittest.TestCase):
 
         if method == MethodType.delete:
             if params_type == ParamsType.data:
-                ret_text = requests.delete(url, headers=header, data=params_body)
+                ret_text = requests.delete(url, headers=header, data=params_body).text
                 if assert_type == AssertType.include:
                     self.assertIn(assert_text, ret_text)
                 elif assert_type == AssertType.equal:
@@ -137,7 +138,7 @@ class RunTest(unittest.TestCase):
                 else:
                     pass
             if params_type == ParamsType.json:
-                ret_text = requests.delete(url, headers=header, json=params_body)
+                ret_text = requests.delete(url, headers=header, json=params_body).text
                 if assert_type == AssertType.include:
                     self.assertIn(assert_text, ret_text)
                 elif assert_type == AssertType.equal:
@@ -151,5 +152,3 @@ if __name__ == '__main__':
         unittest.main(
             testRunner=xmlrunner.XMLTestRunner(output=output),
             failfast=False, buffer=False, catchbreak=False)
-
-
